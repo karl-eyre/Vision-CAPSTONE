1365778bacac890b0faa4a005279a79a4c5f4f72
Slexx <slex-@hotmail.com>
2020-08-22 15:38:12 +1000

    Ficed ActionMap

--- Assets/InDevelopment/ActionMap/GameControls.cs
+++ Assets/InDevelopment/ActionMap/GameControls.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/InDevelopment/Mechanics/ActionMap/GameControls.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/InDevelopment/ActionMap/GameControls.inputactions'
 
 using System;
 using System.Collections;
