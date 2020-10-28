4f9b53d16343b51da5ca9f912d58a1594431cd3b
Kyan Rowse <rowse9@gmail.com>
2020-10-28 12:15:56 +1100

    ReverbZones

---
+++ Assets/FMOD Scripts/DistanceReverb.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class DistanceReverb : MonoBehaviour
+{
+    EventInstance reverb;
+    GameObject player;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+
+        reverb = RuntimeManager.CreateInstance("snapshot:/MainRoom/WholeRoom");
+        RuntimeManager.AttachInstanceToGameObject(reverb, transform, GetComponent<Rigidbody>());
+        reverb.start();
+        reverb.release();
+    }
+    private void OnDestroy()
+    {
+        reverb.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+    }
+
+    void GetDistance()
+    {      
+        float dist = Vector3.Distance(transform.position, player.transform.position);
+        Debug.Log(dist);
+    }
+}
