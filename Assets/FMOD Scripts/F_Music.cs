cc09c62aa8f5fd11ddc019ee18059899fa9f55ab
Kyan Rowse <rowse9@gmail.com>
2020-10-28 20:33:23 +1100

    Auto stash before merge of "master" and "origin/master"

--- Assets/FMOD Scripts/F_Music.cs
+++ Assets/FMOD Scripts/F_Music.cs
@@ -1,15 +1,27 @@
 ﻿using FMOD.Studio;
 using FMODUnity;
+using InDevelopment.Alex;
 using UnityEngine;
 
 public class F_Music : MonoBehaviour
 {
     public static EventInstance music;
+    string enemyState;
 
     void Start()
     {
         music = RuntimeManager.CreateInstance("event:/Music/AmbientMusic");
         music.start();
         music.release();
+
+        StateManager.changeStateEvent += enemyState;
+    }
+
+    void MusicSwitcher()
+    {      
+        if (enemyState == "")
+        {
+
+        }
     }
 }
