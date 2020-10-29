45d92179e9a7214a3b7a9bb0ce20e1b0258bd68c
Kyan Rowse <rowse9@gmail.com>
2020-10-29 13:16:46 +1100

    musicchanging

--- Assets/FMOD Scripts/F_Music.cs
+++ Assets/FMOD Scripts/F_Music.cs
@@ -1,6 +1,7 @@
 ﻿using FMOD.Studio;
 using FMODUnity;
 using InDevelopment.Alex;
+using InDevelopment.Alex.EnemyStates;
 using UnityEngine;
 
 public class F_Music : MonoBehaviour
@@ -13,16 +14,5 @@ public class F_Music : MonoBehaviour
         music = RuntimeManager.CreateInstance("event:/Music/AmbientMusic");
         music.start();
         music.release();
-
-        StateManager.changeStateEvent += MusicSwitcher;
-    }
-
-    void MusicSwitcher(string enemyState)
-    {
-        
-        if (enemyState == "InDevelopment.Alex.EnemyStates.StationaryEnemyState")
-        {
-            Debug.Log("Patroliing");
-        }    
     }
 }
---
+++ Assets/FMOD Scripts/F_MusicChange.cs
@@ -0,0 +1,32 @@
+﻿using InDevelopment.Alex;
+using InDevelopment.Alex.EnemyStates;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class F_MusicChange : MonoBehaviour
+{
+    EnemyController enemyController;
+    void Start()
+    {
+        enemyController = GameObject.Find("Enemy").GetComponent<EnemyController>();
+
+        StateManager.changeStateEvent += MusicSwitcher;
+    }
+    void MusicSwitcher(EnemyStateBase enemyState)
+    {
+        if (enemyState == enemyController.patrollingEnemyState)
+        {
+            F_Music.music.setParameterByName("MusicState", 0f, false);
+        }
+        else if (enemyState == enemyController.playerDetectedState)
+        {
+            F_Music.music.setParameterByName("MusicState", 1f, false);
+        }
+        else if (enemyState == enemyController.playerDetectedState)
+        {
+            F_Music.music.setParameterByName("MusicState", 2f, false);
+        }
+    }
+}
