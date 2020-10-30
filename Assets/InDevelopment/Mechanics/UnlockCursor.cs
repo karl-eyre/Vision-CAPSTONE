1a1c9e2df27ffadbd5994c7a31e372ad34905170
Karl Eyre <karl_eyre@hotmail.com>
2020-10-30 15:38:47 +1100

    Glass Colour Change

---
+++ Assets/InDevelopment/Mechanics/UnlockCursor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockCursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+
+}
