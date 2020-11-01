279c9221c0beeb30927aacbb95335d26a7923839
Karl Eyre <karl_eyre@hotmail.com>
2020-11-02 02:21:18 +1100

    Main Menu Setup

---
+++ Assets/InDevelopment/Mechanics/MenuCinemachine.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class MenuCinemachine : MonoBehaviour
+{
+    public GameControls controls;
+    public CinemachineVirtualCamera activeCam;
+    public CinemachineVirtualCamera mainCam;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        mainCam = activeCam;
+        activeCam.Priority++;
+    }
+
+    private void Start()
+    {
+        SetupControls();
+    }
+
+    private void SetupControls()
+    {
+        controls = new GameControls();
+        controls.Enable();
+        controls.Menu.OpenPauseMenu.performed += Escape;
+    }
+
+
+    //return to mainMenu
+    public void Escape(InputAction.CallbackContext obj)
+    {
+        activeCam.Priority--;
+
+        activeCam = mainCam;
+
+        activeCam.Priority++;
+    }
+
+    //Change Camera
+    public void ChangeCam(CinemachineVirtualCamera target)
+    {
+        activeCam.Priority--;
+
+        activeCam = target;
+
+        activeCam.Priority++;
+    }
+
+}
