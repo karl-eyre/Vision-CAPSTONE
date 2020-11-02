8753c40a9898c64a8c5b1c03159cdec4205fa8c6
Karl Eyre <karl_eyre@hotmail.com>
2020-11-03 00:14:28 +1100

    Added Main Menu Functions

---
+++ Assets/InDevelopment/Mechanics/Menu/MatchAspect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MatchAspect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        RectTransform rt = GetComponent<RectTransform>();
+        float canvasHeight = rt.rect.height;
+        float desiredCanvasWidth = canvasHeight * Camera.main.aspect;
+        rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, desiredCanvasWidth);
+    }
+}
---
+++ Assets/InDevelopment/Mechanics/Menu/MenuManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+namespace InDevelopment.Mechanics
+{
+    public class MenuManager : MonoBehaviour
+    {
+        public GameControls controls;
+
+        private void Start()
+        {
+            SetupControls();
+        }
+
+        private void SetupControls()
+        {
+            controls = new GameControls();
+            controls.Enable();
+            controls.Menu.OpenPauseMenu.performed += Escape;
+        }
+        
+        public void Escape(InputAction.CallbackContext obj)
+        {
+            if(SceneManager.GetActiveScene().name == "MainMenu")
+            {
+                ExitGame();
+            }
+            else
+            {
+                MainMenu();
+            }
+        }
+
+        public void ExitGame()
+        {        
+            Application.Quit();
+            Debug.Log("We Finished");
+        }
+
+        public void MainMenu()
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+
+        public void Credits()
+        {
+            SceneManager.LoadScene("Credits");
+        }
+
+        public void Settings()
+        {
+            SceneManager.LoadScene("Settings");
+        }
+
+        public void StartGame()
+        {
+            SceneManager.LoadScene("1_TutorialRooms");
+        }
+    }
+}
