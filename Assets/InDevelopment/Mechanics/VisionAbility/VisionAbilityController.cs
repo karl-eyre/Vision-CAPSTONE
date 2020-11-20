947445dfe8c2af57dec3b64212bca5c862036ef5
Dylan <dylanwebb5@hotmail.com>
2020-11-20 11:50:25 +1100

    UI Animation Changes

--- Assets/InDevelopment/Mechanics/VisionAbility/VisionAbilityController.cs
+++ Assets/InDevelopment/Mechanics/VisionAbility/VisionAbilityController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
 
-
 namespace InDevelopment.Mechanics.VisionAbility
 {
     public class VisionAbilityController : AbilityBase
