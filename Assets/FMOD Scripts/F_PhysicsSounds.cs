34d0c904888a5257a839e1caa56394bb55e7e7c9
Kyan Rowse <rowse9@gmail.com>
2020-12-14 19:33:27 +1100

    CUP SOUNDS

---
+++ Assets/FMOD Scripts/F_PhysicsSounds.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
+
+public class F_PhysicsSounds : MonoBehaviour
+{
+    private bool impact;
+
+    [FMODUnity.EventRef]
+    public string eventPath;
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        if (coll.relativeVelocity.magnitude > 3.5)
+        {
+            RuntimeManager.PlayOneShotAttached(eventPath, this.gameObject);
+        }
+    }
+
+    IEnumerator ImpactReset()
+    {
+        yield return new WaitForSeconds(1);
+        impact = false;
+
+    }
+}
